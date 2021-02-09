18660b5febd456cac509494d2716d73ab02c753e
Srijarko Roy <66861243+srijarkoroy@users.noreply.github.com>
2021-02-09 13:46:08 +0530

    Update data_gen.py

--- code/data_gen.py
+++ code/data_gen.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.datasets import make_gaussian_quantiles
 
-def dataset(n,random_seed):
+def dataset(n,random_seed,classes):
   if random_seed:
     np.random.seed(random_seed)
-    X, y = make_gaussian_quantiles(n_samples=n, n_features=2, n_classes=2)
+    X, y = make_gaussian_quantiles(n_samples=n, n_features=2, n_classes=classes)
     return X, y*2-1
