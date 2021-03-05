124cc798f7dcb2a797e513f35d41c20e74f62120
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2021-03-05 16:14:39 +0530

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt              #importing the required modules
 from datagenerate import dataset
 from plot import plot_adaboost
 from boosting import AdaBoost
 
 def truncated_adaboost(clf, t: int):         #Truncate a fitted AdaBoost up to (and including) a particular iteration
-  assert t > 0     #t must be a positive integer
+  assert t > 0                               #t must be a positive integer
   from copy import deepcopy
   new_clf = deepcopy(clf)
   new_clf.stumps = clf.stumps[:t]
@@ -13,26 +13,26 @@ def truncated_adaboost(clf, t: int):         #Truncate a fitted AdaBoost up to (
 
 def plot_iter_adaboost(X, y, clf, iters):       #Plot weak learner and cumulaive strong learner at each iteration.
    
-  # larger grid
-  fig, axes = plt.subplots(figsize=(8, iters*3),
+  
+  fig, axes = plt.subplots(figsize=(8, iters*3),        #larger grid
                             nrows=iters,
                             ncols=2,
                             sharex=True,
                             dpi=100)
   fig.set_facecolor('white')
 
-  _ = fig.suptitle('Decision boundaries after every iteration')
+  _ = fig.suptitle('Decision boundaries after every iteration')   #Plot the decision boundaries
   for i in range(iters):
     ax1, ax2 = axes[i]
 
-    # Plot weak learner
-    _ = ax1.set_title(f'Weak learner at iteration {i + 1}')
+    
+    _ = ax1.set_title(f'Weak learner at iteration {i + 1}')     #Plot weak learner
     plot_adaboost(X, y, clf.stumps[i],
                   sample_weights=clf.sample_weights[i],
                   annotate=False, ax=ax1)
 
-    # Plot strong learner
-    trunc_clf = truncated_adaboost(clf, t = i + 1)
+   
+    trunc_clf = truncated_adaboost(clf, t = i + 1)              #Plot strong learner
     _ = ax2.set_title(f'Strong learner at iteration {i + 1}')
     plot_adaboost(X, y, trunc_clf,
                   sample_weights=clf.sample_weights[i],
